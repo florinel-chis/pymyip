f1a3f87d28181cdc1fbacf7553f3946e5c194381
Florinel Chis <florinel.chis@gmail.com>
2012-12-27 17:08:29 +0000

    chaged message, added headers string (remote_Addr is a load balancer) with check if key exists

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import os
-import cgi
 
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
 
-#@app.before_request
-#def get_remote_addr():
-#    return request.remote_addr
-
 @app.route('/')
 def hello():
-    s = request.headers.__repr__()
-    s += 'Hello! Your IP is '
-    s +=request.remote_addr    
+    #s = request.headers.__repr__()
+    s = 'Hello! Your IP is '
+    
+    if 'X-Forwarded-For' in request.headers:
+        s += request.headers['X-Forwarded-For']
+    else:
+        s += request.remote_addr
+        
     return s
 
 if __name__ == '__main__':
