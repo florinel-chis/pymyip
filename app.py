c794ed7972aabfe056c2298e20c71cfcfdda529d
Florinel Chis <florinel.chis@gmail.com>
2012-12-27 17:13:36 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():    
+    #s = request.headers.__repr__()    
+    s = 'Hello! Your IP is '
+    #Heroku remote_addr is actually the load balancer in front of the web server so it won't contain the proper ip address
+    #display instead X-Forwarded-For which contains the actual ip address
+    if 'X-Forwarded-For' in request.headers:
+        s += request.headers['X-Forwarded-For']
+    else:
+        s += request.remote_addr
+        
+    return s
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
