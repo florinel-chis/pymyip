fa7899dde940a57b7b9f963c7fc7b25e815668f5
Florinel Chis <florinel.chis@gmail.com>
2012-12-27 16:49:25 +0000

    chaged message, added ip - fixed

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
 import cgi
+
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
+#@app.before_request
+#def get_remote_addr():
+#    return request.remote_addr
+
 @app.route('/')
 def hello():
-    return 'Hello World modified! Your IP is '.cgi.escape(os.environ["REMOTE_ADDR"])
+    s= 'Hello! Your IP is '
+    s+=request.remote_addr    
+    return s
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
